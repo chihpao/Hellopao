1b2daa680c9a9b19131d7a980c2aa98284c5caa5
chihpao <chihpaoo@email.com>
2024-05-28 08:22:04 +0800

    first commit

--- components/AppHeader.js
+++ components/AppHeader.js
@@ -1,7 +1,23 @@
 export default function AppHeader({ title, description, children }) {
     return (
-        <header>
-            <div className="container">
+        <header style={{ 
+            backgroundImage: 'url(https://i.imgur.com/mLvApYc.jpeg)', 
+            backgroundPosition: 'center',
+            backgroundRepeat: 'no-repeat',
+            backgroundSize: 'cover',
+            height: '1024px',
+            position: 'relative'
+            }}>
+            <div className="container" style={{
+                backgroundColor: 'rgba(0, 0, 0, 0.5)',
+                color: 'white',
+                padding: '20px',
+                borderRadius: '8px',
+                position: 'absolute', // 設置container為絕對定位
+                top: '20%', // 將container置於header垂直中心
+                left: '50%', // 將container置於header水平中心
+                transform: 'translate(-50%, -50%)' // 調整container位置
+            }}>
                 <h1>{title}</h1>
                 <p>{description}</p>
                 {children}
--- components/Navbar.js
+++ components/Navbar.js
@@ -1,6 +1,6 @@
 "use client";
 import { useState, useEffect } from 'react';
-import { usePathname } from 'next/navigation'
+import { usePathname } from 'next/navigation';
 import Link from 'next/link';
 
 export default function Navbar() {
@@ -9,22 +9,23 @@ export default function Navbar() {
 
     useEffect(() => {
         setShowMenu(false);
-    }, [pathname])
-
+    }, [pathname]);
 
     return (
-        <nav className={`navbar ${showMenu ? "show-menu" : "hide-menu"} bg-slate-100 h-[56px] w-full fixed z-[1000] shadow-sm px-5`}>
+        <nav className={`navbar ${showMenu ? "show-menu" : ""} bg-slate-100 h-[56px] w-full fixed z-[1000] shadow-sm px-5`}>
             <div className="flex h-full justify-between items-center">
-                <Link className="text-lg font-bold" href="/">My Website</Link>
-                <div className="menu bg-slate-100">
-                    <Link href="/create-product" className="mr-3 hover:opacity-50 block sm:inline-block">Create Product</Link>
-                    <Link href="/chat-with-ai" className="mr-3 hover:opacity-50 block sm:inline-block">Chat with AI</Link>
-                    <Link href="" className="mr-3 hover:opacity-50 block sm:inline-block">Login</Link>
-
+                <div className={`menu ${showMenu ? "block" : "hidden"} sm:flex bg-slate-100`}>
+                    <Link href="/create-product" className="mr-3 hover:opacity-50 block">Create Product</Link>
+                    <Link href="/chat-with-ai" className="mr-3 hover:opacity-50 block">Chat with AI</Link>
+                    <Link href="/login" className="mr-3 hover:opacity-50 block">Login</Link>
                 </div>
+                <div className="flex-grow"></div>
+                <Link className="text-lg font-bold pr-5" href="/">FUN</Link>
                 <button onClick={() => setShowMenu(!showMenu)}
-                    className="menu-btn block sm:hidden rounded-md hover:opacity-70 active:opacity-50"
+                    className="menu-btn block rounded-md hover:opacity-70 active:opacity-50"
                 >
+                    <span className="line"></span>
+                    <span className="line"></span>
                     <span className="line"></span>
                 </button>
             </div>
